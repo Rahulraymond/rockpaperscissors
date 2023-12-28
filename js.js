c4ede64c726b7681e6a8395b75ac75036380eef8
Rahulraymond <63040146+Rahulraymond@users.noreply.github.com>
2023-12-28 12:10:37 +0530

    the UI for Rock paper scissors ready and works well

--- js.js
+++ js.js
@@ -1,6 +1,18 @@
-let playerScore = 0;
-let computerScore = 0;
-let rounds = 5;
+let playerScore = 0
+let computerScore = 0
+// let rounds = 5;
+let rock = document.getElementById("rock")
+let paper = document.getElementById("paper")
+let scissors = document.getElementById("scissors")
+let btn = document.querySelectorAll('.btn')
+let playBtnContainer = document.getElementsByClassName("play-btns")
+let container = document.querySelector(".container")
+let restartBlock = document.getElementById("restart-block")
+let restartBtn = document.getElementById("restart")
+let computerScores =  document.getElementById("computerScore")
+let playerScores = document.getElementById("playerScore")
+// let playerTurn = btn.forEach((button)=>button.addEventListener("click",()=>{ let buttonId = button.id
+//     console.log(buttonId)}))
 
 function computerPlay() {
     const choices = ["rock", "paper", "scissors"];
@@ -8,40 +20,99 @@ function computerPlay() {
     return choices[randomIndex];
 }
 
-function playerPlay() {
-    const input = prompt("Enter: ").toLowerCase();
-    return input;
-}
-
+// function playerPlay() {
+//     const input = prompt("Enter: ").toLowerCase();
+//     return input;
+// }
+let div = document.createElement("div")
+div.classList.add("results")
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "It's a draw! You both chose " + playerSelection;
+        div.innerText="It's a draw! You both chose " + playerSelection
+        // container.appendChild(div)
     } else if (
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissors" && computerSelection === "paper")
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")
     ) {
-        playerScore++;
-        return "You win! " + playerSelection + " beats " + computerSelection;
+        // document.getElementById("playerScore").innerText = playerScore++
+        div.innerText= "You win! " + playerSelection + " beats " + computerSelection
+        // container.appendChild(div)
+
+        playerScores.innerText = playerScore++
     } else {
-        computerScore++;
-        return "You lose! " + computerSelection + " beats " + playerSelection;
+       computerScores.innerText =  computerScore++
+        div.innerText=  "You lose! " + computerSelection + " beats " + playerSelection
+        // container.appendChild(div)
+
     }
+    container.appendChild(div)
+}
+// document.getElementById("playerScore").innerText = playerScore++
+restartBlock.style.display = "none";
+btn.forEach((button)=>button.addEventListener("click",()=>{
+    if(playerScore ==2){
+        div.textContent = `You WIN with ${playerScore}, Computer Looses with ${computerScore}`
+        restartBlock.style.display = "flex";
+    }
+    else if (computerScore ==2){
+        div.textContent = `You Loose with ${playerScore}, Computer WINS with ${computerScore}`
+        restartBlock.style.display = "flex";
+    }else{
+        let buttonId = button.id
+    playRound(buttonId.toString(), computerPlay())
+    }
+
+    div.appendChild(div)
+
+
+    }))
+let restart = () =>{
+    playerScore = 0
+    computerScore = 0
+    playerScores.innerText = 0
+    computerScores.innerText = 0
+    restartBlock.style.display = "none";
+    div.innerText = ""
+    // restartBlock.classList.add("restart-block")
+    // restartBlock.classList.remove("restart-hide")
 }
 
-function game() {
-    for (let i = 0; i < rounds; i++) {
-        const playerSelection = playerPlay();
-        const computerSelection = computerPlay();
-        const result = playRound(playerSelection, computerSelection);
-        console.log(result);
-    }
+restartBtn.addEventListener("click",restart)
 
-    // After the game ends, display the final scores
-    console.log("Final Scores:");
-    console.log("You: " + playerScore);
-    console.log("Computer: " + computerScore);
-}
+
+
+
+
+
+
+
+
+// function playerPlay(player1){
+//     let playerChoice = player1
+//     let computerChoice = computerPlay()
+//     let result = playRound(playerChoice, computerChoice)
+//     console.log(result)
+
+// }
+// console.log(playerPlay())
+// const playerSelection = playerPlay();
+// const computerSelection = computerPlay();
+// const result = playRound(playerSelection, computerSelection);
+// btn.addEventListener("click",()=>console.log(playRound))
+// function game() {
+//     for (let i = 0; i < rounds; i++) {
+//         const playerSelection = playerPlay();
+//         const computerSelection = computerPlay();
+//         const result = playRound(playerSelection, computerSelection);
+//         console.log(result);
+//     }
+
+//     // After the game ends, display the final scores
+//     console.log("Final Scores:");
+//     console.log("You: " + playerScore);
+//     console.log("Computer: " + computerScore);
+// }
 
 // Start the game
-game();
+// game();
